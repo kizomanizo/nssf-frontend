f4b419ea1bb775225e358bf0ff69d3e51b7070e7
Kizito M <kizomanizo@gmail.com>
2023-06-17 16:32:59 +0300

    Initiated v1.0.0 Frintend

---
+++ README.md
@@ -0,0 +1,15 @@
+# NSSF Frontend
+
+A barebones HTML, JS and CSS app that sends NSSF member number to NSSF servers through a CORS decoupling backend in order to retrieve member contribution balance totals.
+
+The utility also shows the number of contributing employers and their names. User details are not shown as that is not part of this ethical hacking adventure that this application is intended-for.
+
+To NSSF, please fix this, this API endpoint should not be a public facing one.
+
+## License
+
+MIT
+
+## Author
+
+Kizito S.M.
---
+++ script.js
@@ -0,0 +1,80 @@
+/* @Author: Kizito - kizomanizo@gmail(dot)com */
+const memberInput = document.getElementById("member_id");
+const checkBalance = document.getElementById("check_balance");
+const backEnd = "http://localhost:4004/nssf/";
+const alertBox = document.getElementById("alert");
+const headingOne = document.getElementById("headingOne");
+const headingTwo = document.getElementById("headingTwo");
+const balHeading = document.getElementById("balHeading");
+const empNumber = document.getElementById("empNumber");
+const rightContent = document.getElementById("right-content");
+
+checkBalance.onclick = function () {
+  // Clear headings and empNumber
+  headingOne.innerHTML = "";
+  headingTwo.innerHTML = "";
+  empNumber.innerHTML = "";
+  balHeading.innerHTML = "Loading...";
+  rightContent.innerHTML = "";
+  getBalance();
+};
+
+// Close the alert box
+async function closeAlert() {
+  alertBox.style.display = "none";
+}
+
+async function getBalance() {
+  try {
+    const response = await fetch(backEnd + memberInput.value);
+    const result = await response.json();
+    if (result.success === false || result.status === 500) {
+      alertBox.innerHTML = `<strong>ERROR: </strong>${result.message}`;
+      alertBox.style.display = "block";
+      setTimeout(closeAlert, 3000);
+    } else if (result.message.data.length === 0) {
+      alertBox.innerHTML = "<strong>ERROR: </strong> Member number invalid.";
+      alertBox.style.display = "block";
+      balHeading.innerHTML = "";
+      setTimeout(closeAlert, 3000);
+    } else {
+      return renderView(result);
+    }
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+async function renderView(result) {
+  const message = result.message;
+  const data = message.data;
+
+  var totalBalance = 0; // Get total constributions
+  for (let i = 0; i < data.length; i++) {
+    totalBalance += Math.floor(parseInt(data[i].AMOUNT));
+  }
+
+  // Count employers
+  const uniqueEmployerId = new Set();
+  data.forEach((contrib) => {
+    uniqueEmployerId.add(contrib.EMPLOYER_ID);
+  });
+  const uniqueEmployerCount = uniqueEmployerId.size;
+
+  // List unique employers
+  const uniqueEmployers = new Map();
+  data.forEach((contrib) => {
+    uniqueEmployers.set(contrib.EMPLOYER_ID, contrib.EMPNAME);
+  });
+
+  headingOne.innerHTML = "Total NSSF Balance"; // Render the view
+  headingTwo.innerHTML = `Member Number: ${memberInput.value}`;
+  balHeading.innerHTML = `${totalBalance.toLocaleString()} Shs.`;
+  empNumber.innerHTML = `Number of Employers: ${uniqueEmployerCount}`;
+  uniqueEmployers.forEach((employerName, employerId) => {
+    const span = document.createElement("span");
+    span.className = "content-line light";
+    span.textContent = `${employerId}:${employerName}`;
+    rightContent.appendChild(span);
+  });
+}
